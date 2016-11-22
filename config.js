b3d536871ca3bcd92bc851cf0a381cb30a006bbe
crash springfield <matthew.boman@gmail.com>
2016-11-22 17:38:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 module.exports = {
 
-  "database": "mongodb://root:kcsw@ds019846.mlab.com:19846/camover", //temporary
+  "database": "", // Mlab database or local Mongo
   "port": process.env.PORT || 3000,
   "secret": "secret", // temporary
-  "transport": "smtps://camover1984%40gmail.com:camoverTEMP420@smtp.gmail.com",
+  "transport": "", // node-mailer transport object
   "baseURL": "http://localhost:3000"
 
 }
