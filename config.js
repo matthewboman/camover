c9ab3931f1077803c2c3587161e874062d30fb85
crash springfield <matthew.boman@gmail.com>
2016-12-16 12:53:03 -0500

    remove config

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-
-  "database": "mongodb://root:kcsw@ds019846.mlab.com:19846/camover", // Mlab database or local Mongo
-  "port": process.env.PORT || 3000,
-  "secret": "secret", // temporary
-  "transport": "smtps://camover1984%40gmail.com:camoverTEMP420@smtp.gmail.com", // node-mailer transport object
-  "baseURL": "http://localhost:3000"
-
-}
